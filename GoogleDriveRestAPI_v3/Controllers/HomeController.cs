0e50086ba3ee06d63b3d4c176c2282e0c508a190
ChinTzu <tzucttan@gmail.com>
2020-01-19 20:54:24 +0800

    My first Commit

---
+++ GoogleDriveRestAPI_v3/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using GoogleDriveRestAPI_v3.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDriveRestAPI_v3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static IHostingEnvironment _env;
+
+        public HomeController(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        [HttpGet]
+        public ActionResult GetGoogleDriveFiles()
+        {
+            return View(GoogleDriveFilesRepository.GetDriveFiles(_env));
+        }
+
+        [HttpPost]
+        public ActionResult DeleteFile(GoogleDriveFiles file)
+        {
+            GoogleDriveFilesRepository.DeleteFile(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        [HttpPost]
+        public ActionResult UploadFile(IFormFile file)
+        {
+            GoogleDriveFilesRepository.FileUpload(file);
+            return RedirectToAction("GetGoogleDriveFiles");
+        }
+
+        public void DownloadFile(string id)
+        {
+            string FilePath = GoogleDriveFilesRepository.DownloadGoogleFile(id);
+
+            Response.ContentType = "application/zip";
+            Response.Headers.Add("content-disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+
+            string path = Path.Combine(_env.ContentRootPath, "~/GoogleDriveFiles/", Path.GetFileName(FilePath));
+            byte[] data = Encoding.ASCII.GetBytes(path);
+            Response.Body.Write(data, 0, data.Length);
+
+            //Response.StatusCode = StatusCodes.Status200OK;
+            Response.Body.FlushAsync();
+        }
+
+        /*
+        public void DownloadFile(string id)
+        {
+            string FilePath = GoogleDriveFilesRepository.DownloadGoogleFile(id);
+
+            Response.ContentType = "application/zip";
+            Response.AddHeader("Content-Disposition", "attachment; filename=" + Path.GetFileName(FilePath));
+            Response.WriteFile(System.Web.HttpContext.Current.Server.MapPath("~/GoogleDriveFiles/" + Path.GetFileName(FilePath)));
+            Response.End();
+            Response.Flush();
+        }
+        */
+
+    }
+}
---
+++ GoogleDriveRestAPI_v3/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoogleDriveRestAPI_v3
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+        public IHostingEnvironment HostingEnvironment { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .Build();
+
+            HostingEnvironment = env;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSingleton<IHostingEnvironment>(HostingEnvironment);
+        }
+
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=GetGoogleDriveFiles}/{Id?}");
+            });
+        }
+    }
+}
